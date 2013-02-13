b0f354b2f99bb21ae14454fba5644b504564a9d5
zhaidangwei@gmail.com <zhaidangwei@gmail.com@be21b841-c6f0-349e-49b9-8c9cdbdf6de0>
2013-02-13 10:24:19 +0000

    [maven-release-plugin]  copy for tag appframework-1.0.0

---
+++ tags/appframework-1.0.0/appframework/src/main/java/org/jiucai/appframework/base/util/PerformanceLogUtil.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package org.jiucai.appframework.base.util;
+
+import java.util.List;
+
+/**
+ * @model PerformanceLogUtil
+ * @description PerformanceLogUtil
+ * @author wangbo
+ * @version 1.0
+ * @date Jan 24, 2013
+ */
+public class PerformanceLogUtil {
+	
+	/**
+	 * @description 格式化性能日志
+	 * @param moduleName 值：关键词竞价, 关键词报表
+	 * @param timeType 计时分类：数据库, 应用, 页面
+	 * @param projectId
+	 * @param accountList
+	 * @param start_time
+	 * @param end_time
+	 * @param all_time
+	 * @return
+	 */
+	public static String formatPerformanceLog(String moduleName,String timeType, Long projectId,List<Long> accountList,String start_time,String end_time,Long all_time ){
+		//service计时输出开始 by wangbo
+		StringBuilder time_log  = new StringBuilder();
+		time_log.append(projectId).append(",");//项目ID
+		for(int i=0;accountList!=null && i<accountList.size();i++){//帐号ID
+			Long accountid = accountList.get(i);
+			time_log.append(accountid);
+			if(i+1<accountList.size()){
+				time_log.append("_");
+			}
+		}
+		time_log.append(",").append(moduleName).append(",");//功能模块
+		if( accountList==null ||accountList.size()==0){//查询类型
+			time_log.append("项目总计").append(",");
+		}else if(accountList.size()>=2){//查询类型
+			time_log.append("多账户查询").append(",");			
+		}else{
+			time_log.append("单账户查询").append(",");
+		}
+		time_log.append(start_time).append(",");//查询开始日期
+		time_log.append(end_time).append(",");//查询结束日期
+		time_log.append(timeType).append(",");//计时分类
+		time_log.append(all_time).append("ms");//花费时间
+		//service计时输出结束 by wangbo
+		return time_log.toString();
+	}
+	/**
+	 * @description main
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
